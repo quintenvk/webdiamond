ef33d2686acda7a2f5e9bfa2a762ae1e5a6e1501
Quinten Van Kerrebroeck <quinten.vankerrebroeck@mediahuis.be>
2023-06-04 20:09:56 +0200

    set crossOrigin

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,11 @@ export default function Index() {
           content="Webdiamond - Frontend development with nodejs, typescript, and more."
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
           rel="stylesheet"
